2ed62f558b058252876e1c9bff400b4192eb16db
Pascal Richet <codeur1design@gmail.com>
2020-09-18 17:13:45 +0200

    creation du dossier

---
+++ Readme.md
@@ -0,0 +1,43 @@
+# Ruby-Game
+
+Ceci est un jeu en langage Ruby dans le style Dungeon's & Dragons en mode texte.
+
+Ce projet est issus d'un exercice inclus dans un cours de chez Openclassrooms.
+
+
+## Instructions
+
+Utilisation des librairies comprises dans le langage Ruby.
+
+Il est recommandé d'apprendre les bases de ces technologies avant d'en modifier le code source.
+
+## Installation
+
+Vous pouvez librement ‘forker' ce projet ou le 'cloner'
+
+Pour installer et utiliser ce projet, un simple navigateur et un éditeur de code feront l'affaire.
+
+## Ressources 
+
+A titre d'exemple
+
+![About Ruby - official website](https://www.ruby-lang.org/fr/documentation/)
+
+Livres sur le sujet
+
+(Liste)
+
+...
+
+## Contribution
+
+Pas de politique de contribution au projet, actuellement.
+Cependant je reste ouvert aux commentaires.
+
+## Licences
+
+MIT - Open source
+
+## Création
+
+Ce projet est créer par: ![Pascal Richet](https://github.com/PascalR2014)
---
+++ starter/jeu.rb
@@ -0,0 +1,171 @@
+class Personne
+  attr_accessor :nom, :points_de_vie, :en_vie
+
+  def initialize(nom)
+    @nom = nom
+    @points_de_vie = 100
+    @en_vie = true
+  end
+
+  def info
+    # A faire:
+    # - Renvoie le nom et les points de vie si la personne est en vie
+    # - Renvoie le nom et "vaincu" si la personne a été vaincue
+  end
+
+  def attaque(personne)
+    # A faire:
+    # - Fait subir des dégats à la personne passée en paramètre
+    # - Affiche ce qu'il s'est passé
+  end
+
+  def subit_attaque(degats_recus)
+    # A faire:
+    # - Réduit les points de vie en fonction des dégats reçus
+    # - Affiche ce qu'il s'est passé
+    # - Détermine si la personne est toujours en_vie ou non
+  end
+end
+
+class Joueur < Personne
+  attr_accessor :degats_bonus
+
+  def initialize(nom)
+    # Par défaut le joueur n'a pas de dégats bonus
+    @degats_bonus = 0
+
+    # Appelle le "initialize" de la classe mère (Personne)
+    super(nom)
+  end
+
+  def degats
+    # A faire:
+    # - Calculer les dégats
+    # - Affiche ce qu'il s'est passé
+  end
+
+  def soin
+    # A faire:
+    # - Gagner de la vie
+    # - Affiche ce qu'il s'est passé
+  end
+
+  def ameliorer_degats
+    # A faire:
+    # - Augmenter les dégats bonus
+    # - Affiche ce qu'il s'est passé
+  end
+end
+
+class Ennemi < Personne
+  def degats
+    # A faire:
+    # - Calculer les dégats
+  end
+end
+
+class Jeu
+  def self.actions_possibles(monde)
+    puts "ACTIONS POSSIBLES :"
+
+    puts "0 - Se soigner"
+    puts "1 - Améliorer son attaque"
+
+    # On commence à 2 car 0 et 1 sont réservés pour les actions
+    # de soin et d'amélioration d'attaque
+    i = 2
+    monde.ennemis.each do |ennemi|
+      puts "#{i} - Attaquer #{ennemi.info}"
+      i = i + 1
+    end
+    puts "99 - Quitter"
+  end
+
+  def self.est_fini(joueur, monde)
+    # A faire:
+    # - Déterminer la condition de fin du jeu
+  end
+end
+
+class Monde
+  attr_accessor :ennemis
+
+  def ennemis_en_vie
+    # A faire:
+    # - Ne retourner que les ennemis en vie
+  end
+end
+
+##############
+
+# Initialisation du monde
+monde = Monde.new
+
+# Ajout des ennemis
+monde.ennemis = [
+  Ennemi.new("Balrog"),
+  Ennemi.new("Goblin"),
+  Ennemi.new("Squelette")
+]
+
+# Initialisation du joueur
+joueur = Joueur.new("Jean-Michel Paladin")
+
+# Message d'introduction. \n signifie "retour à la ligne"
+puts "\n\nAinsi débutent les aventures de #{joueur.nom}\n\n"
+
+# Boucle de jeu principale
+100.times do |tour|
+  puts "\n------------------ Tour numéro #{tour} ------------------"
+
+  # Affiche les différentes actions possibles
+  Jeu.actions_possibles(monde)
+
+  puts "\nQUELLE ACTION FAIRE ?"
+  # On range dans la variable "choix" ce que l'utilisateur renseigne
+  choix = gets.chomp.to_i
+
+  # En fonction du choix on appelle différentes méthodes sur le joueur
+  if choix == 0
+    joueur.soin
+  elsif choix == 1
+    joueur.ameliorer_degats
+  elsif choix == 99
+    # On quitte la boucle de jeu si on a choisi
+    # 99 qui veut dire "quitter"
+    break
+  else
+    # Choix - 2 car nous avons commencé à compter à partir de 2
+    # car les choix 0 et 1 étaient réservés pour le soin et
+    # l'amélioration d'attaque
+    ennemi_a_attaquer = monde.ennemis[choix - 2]
+    joueur.attaque(ennemi_a_attaquer)
+  end
+
+  puts "\nLES ENNEMIS RIPOSTENT !"
+  # Pour tous les ennemis en vie ...
+  monde.ennemis_en_vie.each do |ennemi|
+    # ... le héro subit une attaque.
+    ennemi.attaque(joueur)
+  end
+
+  puts "\nEtat du héro: #{joueur.info}\n"
+
+  # Si le jeu est fini, on interompt la boucle
+  break if Jeu.est_fini(joueur, monde)
+end
+
+puts "\nGame Over!\n"
+
+# A faire:
+# - Afficher le résultat de la partie
+
+if joueur.en_vie
+  puts "Vous avez gagné !"
+else
+  puts "Vous avez perdu !"
+end
+
+
+
+
